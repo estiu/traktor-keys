1efe20b60a5e7efbc3c7ae90d5fae8c270b98daa
Víctor M. Valenzuela <vemv@vemv.net>
2015-04-23 07:25:22 +0200

    claudio 0.1.3

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"] [claudio "0.1.2"]])
+  :dependencies [[org.clojure/clojure "1.5.1"] [claudio "0.1.3"]])
